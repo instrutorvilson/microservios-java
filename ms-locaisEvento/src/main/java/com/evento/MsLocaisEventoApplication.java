ac478341c4ec069020eb1babc2fd173054607957
Vilson <instrutorvilsonmoro@gmail.com>
2022-06-04 17:09:56 -0300

    Configurado microservice local evento com cliente eureka

--- ms-locaisEvento/src/main/java/com/evento/MsLocaisEventoApplication.java
+++ ms-locaisEvento/src/main/java/com/evento/MsLocaisEventoApplication.java
@@ -2,7 +2,9 @@ package com.evento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MsLocaisEventoApplication {
 
